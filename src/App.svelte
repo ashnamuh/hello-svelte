f02844b8d2f3656c44a7b26e41917e51ef612cfd
Jaemok <ashnamuh@gmail.com>
2019-08-06 09:59:57 +0900

    add ArrayList.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -2,8 +2,19 @@
 import TheHeader from './components/TheHeader.svelte';
 import Count from './components/Count.svelte';
 import Event from './components/Event.svelte';
+import Props from './components/Props.svelte';
+import ArrayList from './components/ArrayList.svelte';
 
 const name = 'ashnamuh';
+
+const ashnamuh = {
+	age: 23
+}
+
+const jaemok = {
+	name: 'Jaemok',
+	age: 23
+}
 
 </script>
 
@@ -17,3 +28,6 @@ const name = 'ashnamuh';
 <h1>Hello {name}!</h1>
 <Count></Count>
 <Event></Event>
+<Props name="ashnamuh" age={ashnamuh.age}></Props>
+<Props {...jaemok}></Props>
+<ArrayList></ArrayList>
---
+++ src/components/ArrayList.svelte
@@ -0,0 +1,29 @@
+<script>
+	let arr = []
+	let obj = {name: 'ashnamuh'}
+
+	const handleClick = () => {
+		// arr = [...arr, 'item']
+		arr.push('item') // Not working
+
+		obj = {...obj, age: 23 }
+		// obj.age = 23 Also not working
+	}
+
+	const handleClick2 = () => {
+		arr = [...arr, 'item']
+		// arr.push('item') // Not working
+
+		obj = {...obj, age: 23 }
+		// obj.age = 23 Also not working
+	}
+</script>
+
+<button on:click={handleClick}>
+	click me to add item
+</button>
+<button on:click={handleClick2}>
+	show me
+</button>
+<p>{arr.join(' ')}</p>
+<p>{Object.values(obj)}</p>
