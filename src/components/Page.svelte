71bb8c1110dfbb57991434d94228005c1c237ee7
Jaemok <ashnamuh@gmail.com>
2019-08-22 18:32:49 +0900

    add Page.svelte

---
+++ src/components/Page.svelte
@@ -0,0 +1,7 @@
+<div>
+  <slot name="header">헤더를 주입해주세요!</slot>
+  <section>
+    섹션 영역
+  </section>
+  <slot name="footer">푸터를 주입해주세요!</slot>
+</div>
--- src/components/Slot.svelte
+++ src/components/Slot.svelte
@@ -1,6 +1,11 @@
 <script>
 import Link from './Link.svelte'
+import Page from './Page.svelte'
 
 </script>
 
 <Link>Link to Svelte!<i class="link" /></Link>
+<Page>
+  <header slot="header">주입하는 헤더</header>
+  <footer slot="footer">주입하는 푸터</footer>
+</Page>
