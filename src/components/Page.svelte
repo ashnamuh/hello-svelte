30cb629982a627e41e3b2abfdca315447f8300ce
Jaemok <ashnamuh@gmail.com>
2019-08-23 10:53:08 +0900

    update Slot.svelte and Page.svelte

--- src/components/Page.svelte
+++ src/components/Page.svelte
@@ -1,7 +1,12 @@
+<script>
+  const username = 'ashnamuh'
+</script>
+
 <div>
-  <slot name="header">헤더를 주입해주세요!</slot>
+  <slot name="header" username={username}>헤더를 주입해주세요!</slot>
   <section>
-    섹션 영역
+    제 이름은 {username}입니다.
+    저는 개발자입니다:)
   </section>
   <slot name="footer">푸터를 주입해주세요!</slot>
 </div>
--- src/components/Slot.svelte
+++ src/components/Slot.svelte
@@ -1,11 +1,18 @@
 <script>
+import { onMount } from 'svelte'
 import Link from './Link.svelte'
 import Page from './Page.svelte'
 
+let element
+
+onMount(() => {
+  console.log(element)
+})
 </script>
 
+<div id="container">container element</div>
 <Link>Link to Svelte!<i class="link" /></Link>
-<Page>
-  <header slot="header">주입하는 헤더</header>
+<Page let:username={pageUsername} bind:this={element}>
+  <header slot="header">{pageUsername}의 소개 페이지!</header>
   <footer slot="footer">주입하는 푸터</footer>
 </Page>
