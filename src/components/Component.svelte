7c23f33655959c4979b2388c3e62e0aa7d988340
Jaemok <ashnamuh@gmail.com>
2019-08-23 12:31:21 +0900

    update Component.svelte

--- src/components/Component.svelte
+++ src/components/Component.svelte
@@ -1,14 +1,27 @@
 <script>
-  import Cat from './Cat.svelte'
-  import Dog from './Dog.svelte'
+import { onMount } from 'svelte'
+import Cat from './Cat.svelte'
+import Dog from './Dog.svelte'
 
-  const options = [
-    { animal: 'cat', component: Cat },
-    { animal: 'dog', component: Dog }
-  ]
+const options = [
+  { animal: 'cat', component: Cat },
+  { animal: 'dog', component: Dog }
+]
 
-  let selected = options[0]
+let selected = options[0]
+let b
+onMount(() => {
+  console.log(b)
+  console.log('b')
+})
+const handleClick = () => console.log('clicked!')
 </script>
+
+<svelte:body bind:this={b} on:click={handleClick} />
+<svelte:head>
+  <title>ashnamuh 개발 블로그</title>
+  <meta name="description" content="ashnamuh의 개발 블로그입니다.">
+</svelte:head>
 
 <select bind:value={selected}>
   <option value={options[0]}>고양이</option>
